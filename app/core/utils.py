bdfb073b68491db940a9224e344bb7bec1fffbae
vmgn16 <mg_619@hotmail.com>
2025-07-18 21:26:39 -0600

    mod parseo update

--- app/core/utils.py
+++ app/core/utils.py
@@ -5,8 +5,10 @@ def is_saludo(text: str) -> bool:
     return any(s in text for s in SALUDOS)
 
 def parse_message(text: str) -> str:
-    text = re.sub(r"\*\*(.*?)\*\*", r"<b>\1</b>", text)
-    text = re.sub(r"\*(.*?)\*", r"<i>\1</i>", text)
+    text = re.sub(r"\*\*(.+?)\*\*", r"<b>\1</b>", text)
+    text = re.sub(r"(?<!\*)\*(?!\*)(.+?)(?<!\*)\*(?!\*)", r"<i>\1</i>", text)
     text = re.sub(r"```(.*?)```", r"<pre>\1</pre>", text, flags=re.DOTALL)
     text = re.sub(r"`(.*?)`", r"<code>\1</code>", text)
+
     return text
+
